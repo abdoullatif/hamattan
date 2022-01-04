aeb4207789d8af42235a3d0705c5c18708584e4b
assooba <abdoullatifsooba7@gmail.com>
2022-01-04 09:50:50 +0000

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,17 @@
+        
+        
+        
+        <!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+            <div class="copyright">
+                <p>Copyright © Designed &amp; Developed by <a href="#" target="_blank">Team Dev</a> 2022</p>
+                <p>Distributed by <a href="" target="_blank">Tulip Industries</a></p> 
+            </div>
+        </div>
+        <!--**********************************
+            Footer end
+        ***********************************-->
+
+        
